98e32bb3b349f96aaae68a0fa1764807a8443de3
Suguna Velury <sugunav@DN0a1f0b34.SUNet>
2023-12-11 13:24:31 -0800

    Faster evaluation

--- evaluate_kernel_smoothing.py
+++ evaluate_kernel_smoothing.py
@@ -24,7 +24,7 @@ rollouts_per_date = 100  # Number of epochs for training
 start_dates = ['1/4/12', '1/5/12', '1/6/12', '1/9/12', '1/9/12']
 date_format = '%m/%d/%y'
 n_tickers = 2
-initial_balance = 10.0
+initial_balance = 10000.0
 max_stocks = 5
 rollout_depth = 20
 
@@ -127,7 +127,7 @@ def rollout_helper(state, depth, date, policy,basis,is_random_policy):
         print(f"Depth:{depth}\n")
         return reward
 '''
-    
+'''   
 def rollout_helper(state, depth, date, policy,basis,is_random_policy):
     if depth == 0:
         return 0
@@ -187,6 +187,75 @@ def rollout_helper(state, depth, date, policy,basis,is_random_policy):
         reward += gamma*rollout_helper(s_prime, depth-1, new_date, policy, basis, is_random_policy)
         #print(f"Depth:{depth}\n")
         return reward
+'''
+def rollout_helper(state, depth, date, policy,basis,is_random_policy):
+    if depth == 0:
+        return 0
+    elif is_random_policy:
+        if state not in policy:
+            action = env.get_next_action(state) ## random action, as epsilon is 0
+            policy[state] = action
+        else:
+            action = policy[state]
+        new_date = date + timedelta(days=7)
+        new_prices = np.zeros(n_tickers)
+        idx = 0
+        for ticker_dict in data.ticker_data:
+            if new_date not in ticker_dict:
+                new_prices[idx] = state.p[idx]*state.prices_bucket_size
+            else:
+                new_prices[idx] = ticker_dict[new_date]
+            idx += 1 
+        s_prime = env.get_next_state(state, action, new_prices)
+        reward = (s_prime.b - state.b)*s_prime.balance_bucket_size + np.sum(s_prime.p*s_prime.prices_bucket_size*s_prime.h) - np.sum(state.p*state.prices_bucket_size*state.h)
+        reward += gamma*rollout_helper(s_prime, depth-1, new_date, policy, basis, is_random_policy)
+        return reward
+    else:
+        if state not in policy:
+            #action = env.get_next_action(state) ## random action, as epsilon is 0
+            #policy[state] = action
+            num_samples=100
+            num_actions = 100
+            np.random.seed(42)
+            random.seed(42)
+            random_actions = [a for a in random.sample(env.possible_actions,num_actions)]
+            #Sample N different states that have the same action as action
+            dtype = [('inner_tuple', [('int1', int), ('int2', int), ('int3', int), ('float1', float), ('float2', float)]), 
+                    ('value', float)]
+            # sampled_basis = np.array([np.random.choice(basis[a],1000) for a in random_actions],dtype=dtype)
+            
+            sampled_basis = np.empty((num_actions, num_samples, 6))
+
+            for i, action in enumerate(random_actions):
+                basis_array = basis[action]
+                indices = np.random.choice(basis_array.shape[0], num_samples, replace=False)
+                sampled_basis[i] = basis_array[indices]
+
+            states = sampled_basis[:,:,:5]
+            q_sa = sampled_basis[:,:,5]
+            net_worth_sk = np.squeeze(states[:,:,0]+ np.sum(states[:,:,1:3]*states[:,:,3:5],axis=-1))
+            net_worth_s= np.array(state.b + np.sum(state.p*state.h))
+            similarity_scores = 1.0/(np.maximum(np.abs(net_worth_s - net_worth_sk),1e-5)) 
+            estimated_q = np.argmax(np.sum((similarity_scores * q_sa)/(np.sum(similarity_scores)),axis=1))
+            action = random_actions[estimated_q]
+
+    
+        else:
+            action = policy[state]
+        new_date = date + timedelta(days=7)
+        new_prices = np.zeros(n_tickers)
+        idx = 0
+        for ticker_dict in data.ticker_data:
+            if new_date not in ticker_dict:
+                new_prices[idx] = state.p[idx]*state.prices_bucket_size
+            else:
+                new_prices[idx] = ticker_dict[new_date]
+            idx += 1 
+        s_prime = env.get_next_state(state, action, new_prices)
+        reward = (s_prime.b - state.b)*s_prime.balance_bucket_size + np.sum(s_prime.p*s_prime.prices_bucket_size*s_prime.h) - np.sum(state.p*state.prices_bucket_size*state.h)
+        reward += gamma*rollout_helper(s_prime, depth-1, new_date, policy, basis, is_random_policy)
+        #print(f"Depth:{depth}\n")
+        return reward
 
 def compute_mean_discounted_reward(basis,learnt_policy,is_random_policy):
     mean_discounted_reward = 0
@@ -213,8 +282,10 @@ learnt_policy = load_policy('q_eps_0.1_rollouts_3000_depth_50.policy')
 #Need to rewrite load_theta in a different file
 with open('basis_dict.pkl','rb') as f:
     basis = pkl.load(f)
+with open('basis_dict_numpy.pkl','rb') as f:
+    basis_np = pkl.load(f)
 #Mixture of SARSA and Kernel smoothing
 random_policy = {} ## empty dict will enforce calling random action each time
 print('Mean Discounted Reward, rollout_depth %s, start_dates %s' % (rollout_depth, start_dates))
-print('Learnt policy: %.2f' % (compute_mean_discounted_reward(basis, learnt_policy, False)))
-print('Random policy: %.2f' % (compute_mean_discounted_reward(basis, random_policy, True)))
+print('Learnt policy: %.2f' % (compute_mean_discounted_reward(basis_np, learnt_policy, False)))
+print('Random policy: %.2f' % (compute_mean_discounted_reward(basis_np, random_policy, True)))
